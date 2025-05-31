509547f58d724ad8ab52036a9e2487bde22f370e
Ethan <ethan.ziegler1@gmail.com>
2025-05-31 19:38:31 -0400

    fix navbar import in layout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
